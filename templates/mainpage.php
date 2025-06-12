01939951a104102164773d42f253549e20c3945e
hubert20 <hubert@tomedia.pl>
2025-06-12 13:22:49 +0200

    Update mainpage.php

--- templates/mainpage.php
+++ templates/mainpage.php
@@ -12,13 +12,15 @@ $contact_cnt = get_field('contact_cnt');
 <!-- Main hero -->
 <?php get_template_part('template-parts/main-hero'); ?>
 
-
-<?php echo wp_kses_post($contact_cnt); ?>
-
 <?php
 while (have_posts()) : the_post();
     the_content(__('Continue reading <span class="meta-nav">&rarr;</span>', 'is-wp-theme'));
 endwhile;
 ?>
+
+
+
+
+<?php echo wp_kses_post($contact_cnt); ?>
 
 <?php get_footer(); ?>
